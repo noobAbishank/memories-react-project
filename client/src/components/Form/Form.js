ba84c51a8fba7400d4665eb4e0931937207b2a75
noobAbishank <nayakabishank1999@gmail.com>
2021-11-23 23:26:22 +0530

    2nd commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,12 +3,13 @@ import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 
 import useStyles from "./styles";
 import { createPost, updatePost } from "../../actions/posts";
  
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ title: "", message: "", tags: "", selectedFile: "" });
+  const [postData, setPostData] = useState({ title: "", message: "", tags: [], selectedFile: "" });
   const dispatch = useDispatch();
   const classes = useStyles();
   const post = useSelector((state) => currentId ? state.posts.posts.find((p) => p._id === currentId) : null);
@@ -16,10 +17,16 @@ const Form = ({ currentId, setCurrentId }) => {
   const history = useHistory();
 
   useEffect(() => {
+    if (!post?.title) clear();
     if(post) setPostData(post);
   }, [post]);
 
-  const handleSubmit = (e) => {
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ title: "", message: "", tags: [], selectedFile: "" })
+  };
+
+  const handleSubmit = async (e) => {
       e.preventDefault();
 
       if(currentId === 0) {
@@ -29,6 +36,14 @@ const Form = ({ currentId, setCurrentId }) => {
         dispatch(updatePost(currentId, { ...postData, name: user?.result?.name}));
         clear();
       }
+  };
+
+  const handleAddChip = (tag) => {
+    setPostData({ ...postData, tags: [...postData.tags, tag] });
+  };
+
+  const handleDeleteChip = (chipToDelete) => {
+    setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== chipToDelete) });
   };
 
   if(!user?.result?.name) {
@@ -41,18 +56,23 @@ const Form = ({ currentId, setCurrentId }) => {
     )
   }
 
-  const clear = () => {
-    setCurrentId(0);
-    setPostData({ title: "", message: "", tags: "", selectedFile: "" })
-  };
-
   return (
     <Paper className={classes.paper} elevation={6}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">{ currentId ? 'Editing' : 'Creating'} a Memory</Typography>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        <TextField name="tags" variant="outlined" label="Tags (comma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+        <div style={{ padding: '5px 0', width: '94%' }}>
+          <ChipInput
+            name="tags"
+            variant="outlined"
+            label="Tags"
+            fullWidth
+            value={postData.tags}
+            onAdd={(chip) => handleAddChip(chip)}
+            onDelete={(chip) => handleDeleteChip(chip)}
+          />
+        </div>
         <div className={classes.fileInput}>
           <FileBase type="File" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})} />
         </div>
