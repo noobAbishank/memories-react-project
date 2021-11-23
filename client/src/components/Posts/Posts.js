98599cdbeff90fd7ae7a1aabdd9cf562ab942c4c
noobAbishank <nayakabishank1999@gmail.com>
2021-11-24 03:26:40 +0530

    5th commit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -13,7 +13,7 @@ const Posts = ({ setCurrentId }) => {
         !posts.length ? <Container align="center"><CircularProgress /></Container>  : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
-                    <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+                    <Grid key={post._id} item xs={12} sm={12} md={6} lg={6}>
                         <Post post={post} setCurrentId={setCurrentId} />
                     </Grid>
                 ))}
